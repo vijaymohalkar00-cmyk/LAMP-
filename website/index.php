d96360921c7f9dd04fdc3061701e516cc6c3a6fe
vijaymohalkar00-cmyk <vijaymohalkar00@gmail.com>
2026-02-16 15:57:11 +0530

    Update index with navigation links

--- website/index.php
+++ website/index.php
@@ -1,4 +1,7 @@
-<?php
-echo "<h1>Welcome to My LAMP Project</h1>";
-echo "<p>Apache, MySQL, PHP are working!</p>";
-?>
+<h1>Welcome to My LAMP Project</h1>
+<p>Apache, MySQL, PHP are working!</p>
+
+<ul>
+  <li><a href="add_user.php">Add User</a></li>
+  <li><a href="users.php">View Users</a></li>
+</ul>
